2c97e27759a570e7a2e287960ea70b36b3e5fb0f
joe-isaac <isaacjoseph950@gmail.com>
2022-08-22 17:01:09 +0300

    task tracker with Vue

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button :style="{background: colour}" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default{
+    name: "Button",
+    props: {
+        text: String,
+        colour: String,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button text="Add task" colour="green" />
+    </header>
+    
+</template>
+
+<script>
+import Button from './Button'
+
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    },
+    components: {
+        Button,
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
